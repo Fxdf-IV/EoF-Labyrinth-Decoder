0f937f521fd5912caef7fdda58015e28beb6cb95
Fxdf-IV <vinicius-c-santos@outlook.com>
2024-12-05 20:11:12 -0300

    "Updated README for executable usage and verified build script"

--- build_installer.py
+++ build_installer.py
@@ -1,13 +1,6 @@
 import PyInstaller.__main__
 import os
 
-# Dependências
-# build_exe_options = {
-#     "packages": ["os", "tkinter"],
-#     "excludes": [],
-#     "include_files": ["README.md"]
-# }
-
 # Configuração do executável
 PyInstaller.__main__.run([
     'src/main.py',
